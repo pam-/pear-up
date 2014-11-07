3267ea5152e3cd7033a86cb28998779b0109499c
pam- <pam.assogba@gmail.com>
2014-11-07 14:06:27 -0500

    going to go ahead and add hidden navbar instead of top menu

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,7 @@
+function mapMaker() {
+	L.mapbox.accessToken = 'pk.eyJ1IjoicGFtLSIsImEiOiJNT09NSzgwIn0.AWl1AY_kO1HMnFHwxb9mww';
+	// geocoder = L.mapbo
+	map = L.mapbox.map('map', 'pam-.jmeb29bh');
+}
+
+window.onload = mapMaker;
